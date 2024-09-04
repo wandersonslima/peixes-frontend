c4455728d11e0d9576fa96276c5ac3565f036dc8
Wanderson <wa4d3rso4@gmail.com>
2024-09-04 15:55:42 -0300

    inicializando projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientesListComponent } from './clientes/clientes-list/clientes-list.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home' , component: HomeComponent},
+  { path: 'clientes-list', component: ClientesListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateComponent } from './template/template.component';
+import { ClientesListComponent } from './clientes/clientes-list/clientes-list.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateComponent,
+    ClientesListComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
